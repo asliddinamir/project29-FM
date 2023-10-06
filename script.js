e59125e5c4519a90451298e092bc5d3ba259993b
Asliddin Amirov <asliddinamir2@gmail.com>
2023-10-06 19:37:03 +0800

    last commit

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const menuButton = document.querySelector(".menu");
 const closeButton = document.querySelector(".close");
+const featureBtn = document.querySelector(".featureBtn");
+const featureContainer = document.querySelector(".feature_container");
+const arrowFeature = document.querySelector("#arrow_feature");
+const companyBtn = document.querySelector(".companyBtn");
+const companyContainer = document.querySelector(".company_container");
+const arrowCompany = document.querySelector("#arrow_company");
 const nav = document.querySelector(".nav");
 const overlay = document.querySelector(".overlay");
 
@@ -12,3 +18,28 @@ closeButton.addEventListener("click", () => {
     nav.classList.remove("show-nav");
     overlay.style.display = "none";
 });
+
+featureBtn.addEventListener('click', () => {
+    if (featureContainer.style.display === 'block') {
+        featureContainer.style.display = 'none'
+        arrowFeature.style.transform = 'rotate(0deg)'
+        featureBtn.style.height = '0'
+    } else {
+        featureContainer.style.display = 'block'
+        arrowFeature.style.transform = 'rotate(180deg)'
+        featureBtn.style.height = '28vh'
+    }
+})
+
+companyBtn.addEventListener('click', () => {
+    if (companyContainer.style.display === 'block') {
+        companyContainer.style.display = 'none'
+        arrowCompany.style.transform = 'rotate(0deg)'
+        companyBtn.style.height = '0vh'
+    } else {
+        companyContainer.style.display = 'block'
+        arrowCompany.style.transform = 'rotate(180deg)'
+        companyBtn.style.height = '23vh'
+    }
+})
+
