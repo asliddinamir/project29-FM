5b108c0329f9352ffab00e39814de1f99f546000
Asliddin Amirov <asliddinamir2@gmail.com>
2023-10-06 17:42:45 +0800

    added overlay

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const menuButton = document.querySelector('.menu');
-const closeButton = document.querySelector('.close');
-const nav = document.querySelector('.nav');
+const menuButton = document.querySelector(".menu");
+const closeButton = document.querySelector(".close");
+const nav = document.querySelector(".nav");
+const overlay = document.querySelector(".overlay");
 
-menuButton.addEventListener('click', () => {
-    nav.classList.add('show-nav');
+menuButton.addEventListener("click", () => {
+    nav.classList.add("show-nav");
+    overlay.style.display = "block";
 });
 
-closeButton.addEventListener('click', () => {
-    nav.classList.remove('show-nav')
-})
+closeButton.addEventListener("click", () => {
+    nav.classList.remove("show-nav");
+    overlay.style.display = "none";
+});
